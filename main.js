c5e23d3ad9ef51ecc38c644fb03d9727ffa8a2be
fatima awad <fatimamuk0@gmail.com>
2019-01-16 18:56:28 -0500

    volume slider added

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 $(document).ready(function(){
+var oo;
+//sign the value of the slider each time its change 
+    $(document).on("input","#myrange",function(){
+         oo= $(this).val()/10;
+       $("#vcontrol h2").text("Volume: "+ oo*10);
+        console.log(oo);
+    });
+  
 var flag=0;
 var btnn=1;
+/*button on and off , when its pressed for the first time it will be turned off , 
+after its turned off will sign another value to the trigger so the next click will turn on */
 $("#onoff").click(function(){
 if (flag==0){
     $("#onoff").animate({left:"77%"},400);
     
     $(".tools h4").text("OFF");
+    $("#volume h2").text("");
     btnn=0;
     flag=1;  
     
@@ -22,7 +33,7 @@ else
     
 }
 });
-
+//assign the keypress of the keyboards to drumpads 
 $(document).keypress(function(e){
     console.log(e.which);
 
@@ -32,7 +43,8 @@ if (e.which==113){
     $("#heater").animate({width: "30%",
     height: "90px"},300);
     if(btnn==1){
-
+        //base on the value of the slider above that we assign its value to oo , adjust the volume of the audio before play it . 
+        $("#Q").prop("volume",oo);
         $("#Q").get(0).play();
         $("#volume h2").text("Heater-2");
     }
@@ -44,6 +56,7 @@ else if(e.which==119){
     $("#dsc").animate({width: "30%",
     height: "90px"},300);
     if(btnn==1){
+        $("#W").prop("volume",oo);
         $("#W").get(0).play();
         $("#volume h2").text("Dsc-Oh");
         }
@@ -54,6 +67,7 @@ else if (e.which==101){
     $("#kick").animate({width: "30%",
     height: "90px"},300);
     if(btnn==1){
+        $("#E").prop("volume",oo);
         $("#E").get(0).play();
         $("#volume h2").text("Kick-n-Hat");
         }
@@ -64,6 +78,7 @@ else if (e.which==97){
     $("#chord3").animate({width: "30%",
     height: "90px"},300);
     if(btnn==1){
+        $("#A").prop("volume",oo);
         $("#A").get(0).play();
         $("#volume h2").text("Chord-3");
         }
@@ -74,6 +89,7 @@ else if (e.which==115){
     $("#dry").animate({width: "30%",
     height: "90px"},300);
     if(btnn==1){
+        $("#S").prop("volume",oo);
         $("#S").get(0).play();
         $("#volume h2").text("Dry-Ohh");
         }
@@ -84,6 +100,7 @@ else if(e.which==100){
     $("#bld").animate({width: "30%",
     height: "90px"},300);
     if(btnn==1){
+        $("#D").prop("volume",oo);
         $("#D").get(0).play();
         $("#volume h2").text("Bld-H1");
         }
@@ -94,6 +111,7 @@ else if(e.which==122){
     $("#punchykick").animate({width: "30%",
     height: "90px"},300);
     if(btnn==1){
+        $("#Z").prop("volume",oo);
         $("#Z").get(0).play();
         $("#volume h2").text("punchy-kick");
         }
@@ -104,6 +122,7 @@ else if(e.which==120){
     $("#sidestick").animate({width: "30%",
     height: "90px"},300);
     if(btnn==1){
+        $("#X").prop("volume",oo);
         $("#X").get(0).play();
         $("#volume h2").text("side-stick-1");
         }
@@ -115,6 +134,7 @@ else if(e.which==99){
     $("#brksnr").animate({width: "30%",
     height: "90px"},300);
     if(btnn==1){
+        $("#C").prop("volume",oo);
         $("#C").get(0).play();
         $("#volume h2").text("Brk-snr");
         }
@@ -125,7 +145,7 @@ else if(e.which==99){
 
 
 
-
+//btnn define wither the button is on or off so its can play the audio or not when clicked 
 
 $("#heater").click(function(){
 $(this).animate({width: "28%",
@@ -134,6 +154,7 @@ $(this).animate({width: "28%",
     height: "90px"},300);
 
     if(btnn==1){
+        $("#Q").prop("volume",oo);
         $("#Q").get(0).play();
         $("#volume h2").text("Heater-2");
     }
@@ -146,6 +167,7 @@ $("#dsc").click(function(){
     $(this).animate({width: "30%",
     height: "90px"},300);
     if(btnn==1){
+        $("#W").prop("volume",oo);
     $("#W").get(0).play();
     $("#volume h2").text("Dsc-Oh");
     }
@@ -157,6 +179,7 @@ $("#kick").click(function(){
     $(this).animate({width: "30%",
     height: "90px"},300);
     if(btnn==1){
+    $("#E").prop("volume",oo);
     $("#E").get(0).play();
     $("#volume h2").text("Kick-n-Hat");
     }
@@ -169,6 +192,7 @@ $("#chord3").click(function(){
     height: "90px"},300);
 
     if(btnn==1){
+    $("#A").prop("volume",oo);
     $("#A").get(0).play();
     $("#volume h2").text("Chord-3");
     }
@@ -181,6 +205,7 @@ $("#dry").click(function(){
     height: "90px"},300);
 
     if(btnn==1){
+    $("#S").prop("volume",oo);
     $("#S").get(0).play();
     $("#volume h2").text("Dry-Ohh");
     }
@@ -192,6 +217,7 @@ $("#bld").click(function(){
     $(this).animate({width: "30%",
     height: "90px"},300);
     if(btnn==1){
+    $("#D").prop("volume",oo);
     $("#D").get(0).play();
     $("#volume h2").text("Bld-H1");
     }
@@ -203,6 +229,7 @@ $("#punchykick").click(function(){
     $(this).animate({width: "30%",
     height: "90px"},300);
     if(btnn==1){
+    $("#Z").prop("volume",oo);
     $("#Z").get(0).play();
     $("#volume h2").text("punchy-kick");
     }
@@ -214,6 +241,7 @@ $("#sidestick").click(function(){
     $(this).animate({width: "30%",
     height: "90px"},300);
     if(btnn==1){
+    $("#X").prop("volume",oo);
     $("#X").get(0).play();
     $("#volume h2").text("side-stick-1");
     }
@@ -225,6 +253,7 @@ $("#brksnr").click(function(){
     $(this).animate({width: "30%",
     height: "90px"},300);
     if(btnn==1){
+    $("#C").prop("volume",oo);
     $("#C").get(0).play();
     $("#volume h2").text("Brk-snr");
     }
